4b468f132f2016f69fd4a044f775a191bd903519
Shawanda Ford <shawanda.ford@gmail.com>
2020-08-07 21:04:09 -0400

    add calculation for scoring and created changes to the highscore html sheet

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,43 +1,45 @@
 //Declare Variables
-var button=document.querySelector("#start")
+var button = document.querySelector("#start")
 var startScreen = document.querySelector("#startScreen")
 var gameBoard = document.querySelector("#gameBoard")
-var timerEl=document.querySelector("#timer")
+var timerEl = document.querySelector("#timer")
 var spanEl = document.querySelector("#question")
 var nextButton = document.querySelector("#next")
 var timer
-var secondsLeft = 20
-var button1=document.querySelector("#button0")
-var button2=document.querySelector("#button1")
-var button3=document.querySelector("#button2")
-var button4=document.querySelector("#button3")
-var answerEl=document.querySelector("#answer")
+var secondsLeft = 10
+var button1 = document.querySelector("#button0")
+var button2 = document.querySelector("#button1")
+var button3 = document.querySelector("#button2")
+var button4 = document.querySelector("#button3")
+var answerEl = document.querySelector("#answer")
+var scoreEl = document.querySelector("#score")
 
-var score
-var count = 0 
+
+var score = 0
+var count = 0
 
 //Set question object with question, options, and the correct answer
 var questions = [
     {
-        question : "Inside which HTML element do we put the Javascript?",
-        choices: ["<javascript>","<js>","<script>","<scripting>"],
+        question: "Inside which HTML element do we put the Javascript?",
+        choices: ["<javascript>", "<js>", "<script>", "<scripting>"],
         answer: "<script>"
-        
+
     },
     {
-        question : "What does CSS stand for?",
-        choices: ["Colorful Style Sheets","Cascading Style Sheets","Computer Style Sheets","Creative Style Sheets"],
+        question: "What does CSS stand for?",
+        choices: ["Colorful Style Sheets", "Cascading Style Sheets", "Computer Style Sheets", "Creative Style Sheets"],
         answer: "Cascading Style Sheets"
     },
     {
-        question :"Which HTML tag is used to define an internal style sheet?",
-        choices: ["<style>","<css>","<script>","<head>"],
+        question: "Which HTML tag is used to define an internal style sheet?",
+        choices: ["<style>", "<css>", "<script>", "<head>"],
         answer: "<style>"
     },
     {
-        question :"How is HTML defined?",
-        choices: ["Hypertext Markup Language","Hyperlinks The Markup Language","Hypertext Madeup Links","None of the Above"],
-        answer: "Hypertext Markup Language"
+        question: "Commonly used data types DO NOT include:",
+        choices: ["strings", "booleans", "alerts", "numbers"],
+        answer: "alerts"
     }
 ]
 
@@ -45,98 +47,112 @@ var questions = [
 //end Game when timer ends
 function endGame() {
     console.log('ended')
+    window.open("highscore.html");
 }
 
 //Timer function
-function timer(){
-    timer=setInterval(
-        function(){
+function timer() {
+    timer = setInterval(
+        function () {
             secondsLeft--
-            timerEl.textContent= "Seconds Left: " + secondsLeft
-            if(secondsLeft===0){
+            timerEl.textContent = "Seconds Left: " + secondsLeft
+            if (secondsLeft === 0) {
                 endGame()
                 clearInterval(timer)
-            }            
+            }
         }, 1000
     )
 }
 
 
-function startGame(){
-   //clear existing html
-    startScreen.setAttribute('style',"display:none;")
+function startGame() {
+    //clear existing html
+    startScreen.setAttribute('style', "display:none;")
     gameBoard.setAttribute('style', 'display:block')
-    
-  
+
+
     //start timer
-   timer()
+    timer()
 
-   //ask first question 
-   spanEl.textContent=questions[count].question 
+    //ask first question 
+    spanEl.textContent = questions[count].question
 
-   //show button options
-   button1.textContent=questions[count].choices[0]
-   button2.textContent=questions[count].choices[1]
-   button3.textContent=questions[count].choices[2]
-   button4.textContent=questions[count].choices[3]
-   console.log(answer)
-        
+    //show button options
+    button1.textContent = questions[count].choices[0]
+    button2.textContent = questions[count].choices[1]
+    button3.textContent = questions[count].choices[2]
+    button4.textContent = questions[count].choices[3]
+    console.log(answer)
+
+    //capture user button clicks
+    button1.addEventListener("click", showAnswer)
+    button2.addEventListener("click", showAnswer)
+    button3.addEventListener("click", showAnswer)
+    button4.addEventListener("click", showAnswer)
+
 
 }
 
 //Present question and answer choices
 
-function question(){
+function question() {
     count++
-   spanEl.textContent=questions[count].question
-   button1.textContent=questions[count].choices[0]
-   button2.textContent=questions[count].choices[1]
-   button3.textContent=questions[count].choices[2]
-   button4.textContent=questions[count].choices[3]
-   console.log(questions[count].answer)
 
-//capture user button clicks
-button1.addEventListener("click",showAnswer)
-button2.addEventListener("click",showAnswer)
-button3.addEventListener("click",showAnswer)
-button4.addEventListener("click",showAnswer)
+    spanEl.textContent = questions[count].question
+    button1.textContent = questions[count].choices[0]
+    button2.textContent = questions[count].choices[1]
+    button3.textContent = questions[count].choices[2]
+    button4.textContent = questions[count].choices[3]
+    console.log(questions[count].answer)
 
-       
+    //capture user button clicks
+    button1.addEventListener("click", showAnswer)
+    button2.addEventListener("click", showAnswer)
+    button3.addEventListener("click", showAnswer)
+    button4.addEventListener("click", showAnswer)
+
+
 }
-    
-function showAnswer(event){
+
+function showAnswer(event) {
     event.preventDefault()
-    
-    
 
-    if(event.target.innerText != questions[count].answer){
-       
-        answerEl.textContent="Incorrect!"
-        
-        
-        
+
+console.log(scoreEl)
+
+    if (event.target.innerText != questions[count].answer) {
+
+
+        answerEl.textContent = "Incorrect!"
+         score=score-10
+
+
+
+
     } else {
-        answerEl.textContent="Correct!"
-        
-        
-       
-    }
-   
+        answerEl.textContent = "Correct!"
+        score=score+10
 
+        
+
+
+        
+    }
+
+scoreEl.textContent="Score:" + score
     //user click
     console.log(event.target.innerText)
+    console.log(scoreEl)
 
     //my answer
     console.log(questions[count].answer)
 }
 
-answerEl.setAttribute('style', 'display:block')
-
-nextButton.addEventListener("click",question)
 
 
+nextButton.addEventListener("click", question)
 
-button.addEventListener("click",startGame)
 
 
-    
+button.addEventListener("click", startGame)
+
